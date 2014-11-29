521cb23f1fbef26203d371220e77fc1e253e54b1
Matt Petty <matt@kizmeta.com>
2014-11-29 00:14:38 -0600

    Refactor Dice Graph and add to_json/to_h

--- lib/dicebag.rb
+++ lib/dicebag.rb
@@ -1,4 +1,5 @@
 require 'singleton'
+require 'json'
 
 module Kernel
   def d
@@ -42,17 +43,9 @@ class DiceBag
   end
 
   def roll(what: self, plus: 0, keep: nil, how_many: nil)
-    puts "Rolling Away..."
-    if what.is_a? Array
-      keep = what.length if keep.nil?
-      DiceSet.new set: what, top: keep, plus: plus
-    elsif what.is_a? Die
-      raise "Must include :how_many if you pass an individual Die" if how_many.nil?
-      keep = how_many if keep.nil?
-      DicePool.new set: how_many.times.collect{ what.clone.reroll }, top: keep, plus: plus
-    else
-      raise what.class.inspect
-    end
+    raise "Must include :how_many if you pass an individual Die" unless how_many
+    keep = how_many if keep.nil?
+    DicePool.new set: how_many.times.collect{ what.clone.reroll }, top: keep, plus: plus
   end
 
 end
@@ -72,6 +65,10 @@ class Die
 
   def inspect; "d#{@sides}#{"+#{@plus}" unless @plus.zero?}=>#{value}"; end
 
+  def to_h
+    {sides: @sides, plus: @plus, value: value}
+  end
+
   def value; @value; end
 
   def roll; @value = rand(1..@sides)+@plus; self end
@@ -86,8 +83,8 @@ end
 
 class DicePool
 
-  attr_reader :how_many, :top_number_of_dice, :results, :set
-  attr_writer :how_many, :top_number_of_dice, :results
+  attr_reader :how_many, :top_number_of_dice, :results, :plus, :set
+  attr_writer :how_many, :top_number_of_dice, :results, :plus
 
   def initialize(set: nil, plus: 0, top: 2)
     @set = set.is_a?(Die) ? [set] : set
@@ -95,18 +92,17 @@ class DicePool
     @plus = plus
     @how_many = 100_000
     @top_number_of_dice = top
-    # generate_results
   end
 
-  def highest(top: @top_number_of_dice, re_roll: false)
+  def highest(top: top_number_of_dice, re_roll: false)
     reroll if re_roll
-    highest_results = @set.sort_by{|d| d.value }.reverse[0..top-1] 
-    return highest_results.map(&:to_i).inject(0){|sum,i| sum += i } + @plus
+    highest_results = set.sort_by{|d| d.value }.reverse[0..top-1] 
+    return highest_results.map(&:to_i).inject(0){|sum,i| sum += i } + plus
   end
   alias :result :highest
 
   def reroll
-    @set.map(&:reroll)
+    set.map(&:reroll)
     self
   end
   
@@ -114,35 +110,16 @@ class DicePool
 
   def generate_results
     @results = Hash.new(0)
-    @how_many.times { @results[highest(top: @top_number_of_dice, re_roll: true)] += 1  }
+    how_many.times { @results[highest(top: top_number_of_dice, re_roll: true)] += 1  }
     self
   end
 
   def graph(r=@results)
-    raise "There are no statistical results to graph. Run :generate_results" if r.empty?
-    data = ""
-    data << "Running #{@how_many} times, taking the top #{@top_number_of_dice}, the breakdown of results by value rolled are:"
-    data << "\n" + ("…" * 100) + "\n"
-    r.sort.each do |total, count| 
-      percentage = (count.to_f/@how_many.to_f)
-      data << sprintf("%3d ", total)
-      data << '('
-      data << sprintf("%5.2f", percentage * 100)
-      data << "%) "
-      dots = (percentage*500).to_i
-      if dots > 100 
-        data << '°' * 80
-        data << "*(#{dots})"
-      else
-        data << '°' * (percentage*500).to_i
-      end
-      data << "\n"
-    end
-    data
+    DiceGraph.new(r, how_many, top_number_of_dice).graph
   end
 
   def roll_results
-    @set.inspect
+    set.inspect
   end
 
   def results
@@ -150,22 +127,68 @@ class DicePool
   end
 
   def total
-    # @set.map(&:value).inject(0){|sum, i| sum += i} + @plus
-    highest(top: @top_number_of_dice)
+    highest(top: top_number_of_dice)
   end
 
   def inspect
-    puts "<DicePool:\n  @set: #{@set.inspect}, @plus: #{@plus}, @total: #{total}, @top: #{@top_number_of_dice}>"
+    puts "<DicePool:\n  @set: #{@set.inspect}, @plus: #{plus}, @total: #{total}, @top: #{top_number_of_dice}>"
   end
 
   def self.inspect 
-    puts "<DicePool:\n  @set: #{@set.inspect}, @plus: #{@plus}, @total: #{total}, @top: #{@top_number_of_dice}>"
+    puts "<DicePool:\n  @set: #{@set.inspect}, @plus: #{plus}, @total: #{total}, @top: #{top_number_of_dice}>"
+  end
+
+  def to_h
+    {
+      top: top_number_of_dice,
+      total: total,
+      plus: plus,
+      set: @set.map(&:to_h)
+    }
+  end
+    
+  def to_json
+    to_h.to_json
   end
 
 end
 
+class DiceGraph
+
+  def initialize(r, how_many, top_number_of_dice)
+    @data = generate_text_graph(r, how_many, top_number_of_dice)
+  end
+
+  def graph
+    @data
+  end
+  
+  def generate_text_graph(r, how_many, top_number_of_dice)
+    raise "There are no statistical results to graph. Run :generate_results" unless r
+    data = "Running #{how_many} times, taking the top #{top_number_of_dice}, the breakdown of results by value rolled are:"
+    data << "\n" + ("◘" * 30) + "\n"
+    r.sort.each do |total, count| 
+      percentage = (count.to_f/how_many.to_f)
+      data << sprintf("%3d ", total)
+      data << '('
+      data << sprintf("%5.2f", percentage * 100)
+      data << "%) "
+      dots = (percentage*500).to_i
+      if dots > 100 
+        data << '▃' * 80
+        data << "*(#{dots})"
+      else
+        data << '▃' * (percentage*500).to_i
+      end
+      data << "\n"
+    end
+    data
+  end
+
+end
 
 Dice = DiceBag.instance 
 
 
 # ds=DiceDSL.parse("3[4d6]"); [ds.reroll.total, ds.reroll.total, ds.reroll.total, ds.reroll.total, ds.reroll.total, ds.reroll.total]
+# h=[]; (2..7).each {|e| (2..7).each {|i| (0..12).each {|plus| 1_000.times {|x| h << DiceDSL.parse("#{e}[#{i}d6]+#{plus}").to_h } } } }
